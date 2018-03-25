1219f927b3cba2069f7d7e2f9ff410d373640753
Vishnu R <vishnurapps@gmail.com>
2018-03-25 11:33:15 +0530

    Function with arguments

---
+++ python/pyboost/02_function.cpp
@@ -0,0 +1,13 @@
+// 01_function.cpp
+#include <iostream>
+#include <boost/python.hpp>
+
+void sayHello( std::string name)
+{
+	std::cout << "Hello " << name;
+}
+
+BOOST_PYTHON_MODULE(my_module)  // Name here must match the name of the final shared library, i.e. my_module.dll or my_module.so
+{
+	boost::python::def("sayHello", &sayHello);
+}
